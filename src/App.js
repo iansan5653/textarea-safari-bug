40a2775694cc720320e8d2be3fec082081caeabd
Ian Sanders <iansan5653@gmail.com>
2024-02-16 13:06:07 -0500

    Add textarea with issue template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [body, setBody] = useState(`## Describe the bug
+  A clear and concise description of what the bug is.
+
+  ## To Reproduce
+  Steps to reproduce the behavior:
+  1. Go to '...'
+  2. Click on '....'
+  3. Scroll down to '....'
+  4. See error
+
+  ## Expected behavior
+  A clear and concise description of what you expected to happen.
+
+  ## Acceptance Criteria
+  - [ ] Please provide a markdown list of requirements that would consider this bug resolved
+
+  ## Screenshots
+  If applicable, add screenshots to help explain your problem.
+
+  ## Additional context
+  Add any other context about the problem here.
+
+  ## Resolution
+  To be filled in by the person working on the ticket - please provide a short summary of the resolution`);
+
+  return <textarea value={body} onChange={(e) => setBody(e.target.value)} />;
 }
 
 export default App;

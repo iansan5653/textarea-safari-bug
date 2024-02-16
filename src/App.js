76a2e23e1c2a78086745f8eb94d523bdca9a40c2
Ian Sanders <iansan5653@gmail.com>
2024-02-16 13:23:36 -0500

    Replace contents with minimal reproduction

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import "./App.css";
 
 function App() {
-  const body = `## Describe the bug
-  A clear and concise description of what the bug is.
-
-  ## To Reproduce
-  Steps to reproduce the behavior:
-  1. Go to '...'
-  2. Click on '....'
-  3. Scroll down to '....'
-  4. See error
-
-  ## Expected behavior
-  A clear and concise description of what you expected to happen.
-
-  ## Acceptance Criteria
-  - [ ] Please provide a markdown list of requirements that would consider this bug resolved
-
-  ## Screenshots
-  If applicable, add screenshots to help explain your problem.
-
-  ## Additional context
-  Add any other context about the problem here.
-
-  ## Resolution
-  To be filled in by the person working on the ticket - please provide a short summary of the resolution`;
+  const body = `a
+b
+c
+d`;
 
   return <textarea>{body}</textarea>;
 }
